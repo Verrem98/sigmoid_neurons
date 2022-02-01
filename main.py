7cd40e7fccc5fc4a50aa55114e2c58a1c749b380
Verrem98 <60145760+Verrem98@users.noreply.github.com>
2022-02-01 23:04:11 +0100

    Update main.py
    typo fix

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class PerceptronLayer:
 
     def get_outputs(self):
         """
-        make a list of all the perceptron outputs of a specific PercentronLayer
+        make a list of all the perceptron outputs of a specific PerceptronLayer
 
         :return: a list of all the outputs of a PerceptronLayer
         """
